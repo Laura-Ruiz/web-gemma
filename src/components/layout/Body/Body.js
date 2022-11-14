887c7d251faf212f3c741f7e46b48aebecae0ac1
Laura-Ruiz <105049695+Laura-Ruiz@users.noreply.github.com>
2022-11-14 10:46:49 +0100

    created header component (#2)

--- src/components/layout/Body/Body.js
+++ src/components/layout/Body/Body.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from '../header/Header'
 import Footer from '../Footer/Footer'
-
+import Sidebar from '../../Sidebar/Sidebar'
 export default function Body({
     children
 }) {
   return (
     <div>
         <Header/>
+        <Sidebar/>
       {children}
        <Footer />
       </div>
--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -1,16 +1,127 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../../styles/Header/Header.css";
+import imgEspana from "../../../assets/icon/espana.png"
+import imgCatalonia from "../../../assets/icon/catalonia.png"
 export default function Header() {
   return (
-    <>
-    <li>
-        <Link to="/">Inicio</Link>
-    </li>
-    <li>
-        <Link to="/Biography">Biografia</Link>
-    </li>
-   
-</>
-  )
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <a class="collapse navbar-collapse" href="/">
+          Gemma Reguant
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <Link class="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/biography">
+                Biografia
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" href="#">
+                Cursos
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" href="#">
+                Recitales
+              </Link>
+            </li>
+            <li class="nav-item dropdown">
+              <Link
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Trayectoria
+              </Link>
+              <ul class="dropdown-menu">
+                <li>
+                  <Link class="dropdown-item" href="#">
+                    Maestra
+                  </Link>
+                </li>
+                <li>
+                  <Link class="dropdown-item" href="#">
+                    Actriz
+                  </Link>
+                </li>
+                <li>
+                  <Link class="dropdown-item" href="#">
+                    Investigadora
+                  </Link>
+                </li>
+                <li>
+                  <Link class="dropdown-item" href="#">
+                    Directora
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/biography">
+                Publicaciones
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/biography">
+                Blog
+              </Link>
+            </li>
+            <li class="nav-item dropdown">
+              <Link
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Galeria
+              </Link>
+              <ul class="dropdown-menu">
+                <li>
+                  <Link class="dropdown-item" href="#">
+                    Fotos
+                  </Link>
+                </li>
+                <li>
+                  <Link class="dropdown-item" href="#">
+                    Videos
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/biography">
+                Contacto
+              </Link>
+            </li>
+           
+          </ul>
+        </div>
+        <div>
+              <img src={imgEspana} alt="bandera españa" style={{width: "25px"}}/>
+              <img src={imgCatalonia} alt="bandera cataluña" style={{width: "25px", height: "18px"}}/>
+            </div>
+      </div>
+    </nav>
+  );
 }
